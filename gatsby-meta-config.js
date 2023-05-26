866392d2c19e76842b4d33e33605f9066693672d
Junyong Moon <fancy.junyongmoon@gmail.com>
2023-05-26 17:12:29 +0900

    url 에러 수정

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -3,13 +3,9 @@ module.exports = {
   description: `Blog posted about ...`,
   author: `purin`,
   introduction: `우리 FISA 네트워킹 모임`,
-  siteUrl: `https:/moneypapers.github.io/`, // Your blog site url
+  siteUrl: `https://moneypapers.github.io/`, // Your blog site url
   social: {
     github: `moneypapers/`, // Your GitHub account
-    medium: ``, // Your Medium account
-    facebook: ``, // Your Facebook account
-    linkedin: ``, // Your LinkedIn account
-    instagram: ``, // Your Instagram account
   },
   icon: `content/assets/felog.png`, // Add your favicon
   keywords: [`blog`],

baa1a9a8fb3dbe7b6ff474581b2a16926c3b4238
Junyong Moon <fancy.junyongmoon@gmail.com>
2023-05-28 17:57:39 +0900

    repo 주소 수정

--- src/components/utterances/index.jsx
+++ src/components/utterances/index.jsx
@@ -4,7 +4,7 @@ import * as Dom from '../../utils/dom'
 import { THEME } from '../../constants'
 
 const src = 'https://utteranc.es/client.js'
-const repo = "hibit-team/hibit-team.github.io"
+const repo = "moneypapers/moneypapers.github.io"
 const branch = 'master'
 const DARK_THEME = 'photon-dark'
 const LIGHT_THEME = 'github-light'
